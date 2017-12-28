9a5bf325f3078580e2783c9caf597813a96d751f
xpwu <xpwu@xpwu.local>
2017-12-29 01:06:15 +0800

    修改wxapp js。支持1.7.0 wxsdk，可以建立两条长连接

--- client/wxapp/stm/wxwebsocket.js
+++ client/wxapp/stm/wxwebsocket.js
@@ -1,55 +1,164 @@
 
 
-function WXWebSocket(url) {
-  this.onmessage = function(data){};
-  this.onopen = function(data){};
-  this.onclose = function(data){};
-  this.onerror = function(data){};
+function TranVersion(vstr) {
+  let ver = vstr.split(".");
+  let version = 0;
+  let factor = 1;
+  for(let i = ver.length; i > 0; --i) {
+    version += ver[i-1] * factor;
+    factor *= 100;
+  }
 
-  this.url = url;
-  this.readyState = WXWebSocket.CONNECTING;
-
-  let that = this;
-
-  wx.connectSocket({
-    url: url
-  });
-
-  wx.onSocketOpen(function(res) {
-    that.readyState = WXWebSocket.OPEN;
-    that.onopen(res);
-  });
-
-  wx.onSocketError(function(res){
-    that.readyState = WXWebSocket.CLOSED;
-    that.onerror(res);
-  });
-
-  wx.onSocketMessage(function(res) {
-    that.onmessage(res);
-  });
-
-  wx.onSocketClose(function(res) {
-    that.readyState = WXWebSocket.CLOSED;
-    that.onclose(res);
-  });
+  return version;
 }
 
-/**
-@param {String|ArrayBuffer|ArrayBufferView|Blob} data
-*/
-WXWebSocket.prototype.send = function(msg) {
-    wx.sendSocketMessage({
-      data:msg
+class WxWebSocketImpl {
+  constructor() {
+    this.onmessage = function(data){};
+    this.onopen = function(data){};
+    this.onclose = function(data){};
+    this.onerror = function(data){};
+
+    this.readyState = WXWebSocket.CONNECTING;
+  }
+
+  /**
+   @param {String|ArrayBuffer|ArrayBufferView|Blob} msg
+   */
+  send(msg){}
+
+  /**
+   @param {number} [code]
+   @param {string} [reason]
+   */
+  close(code, reason){}
+}
+
+class WxWebSocketImplless170 extends WxWebSocketImpl {
+  constructor(url) {
+    super();
+
+    wx.connectSocket({
+      url: url
     });
-};
-/**
-@param {number} [code]
-@param {string} [reason]
-*/
-WXWebSocket.prototype.close = function(code,reason) {
-    wx.closeSocket();
-};
+
+    wx.onSocketOpen((res)=>{
+      this.readyState = WXWebSocket.OPEN;
+      this.onopen(res);
+    });
+
+    wx.onSocketError((res)=>{
+      this.readyState = WXWebSocket.CLOSED;
+      this.onerror(res);
+    });
+
+    wx.onSocketMessage((res)=>{
+      this.onmessage(res);
+    });
+
+    wx.onSocketClose((res)=>{
+      this.readyState = WXWebSocket.CLOSED;
+      this.onclose(res);
+    });
+  }
+
+  send(msg) {
+    wx.sendSocketMessage({
+      data: msg
+    });
+  }
+
+  close(code,reason) {
+    wx.closeSocket({code:code, reason:reason});
+  };
+}
+
+class WxWebSocketImpl170 extends WxWebSocketImpl {
+  constructor(url) {
+    super();
+
+    this.task = wx.connectSocket({
+      url: url
+    });
+
+    this.task.onOpen((res)=>{
+      this.readyState = WXWebSocket.OPEN;
+      this.onopen(res);
+    });
+
+    this.task.onClose((res)=>{
+      this.readyState = WXWebSocket.CLOSED;
+      this.onclose(res);
+    });
+
+    this.task.onError((res)=>{
+      this.readyState = WXWebSocket.CLOSED;
+      this.onerror(res);
+    });
+
+    this.task.onMessage((res)=>{
+      this.onmessage(res);
+    });
+  }
+
+  send(msg) {
+    this.task.send({
+      data: msg
+    });
+  }
+
+  close(code,reason) {
+    this.task.close({code:code, reason:reason});
+  };
+}
+
+
+class WXWebSocket {
+
+  constructor(url) {
+    let res = wx.getSystemInfoSync();
+    if (res.SDKVersion && TranVersion(res.SDKVersion) >= 10700) {
+      this.impl_ = new WxWebSocketImpl170(url);
+    } else  {
+      this.impl_ = new WxWebSocketImplless170(url);
+    }
+  }
+
+  set onmessage(f) {
+    this.impl_.onmessage = f;
+  }
+
+  set onopen(f) {
+    this.impl_.onopen = f;
+  }
+
+  set onclose(f) {
+    this.impl_.onclose = f;
+  }
+
+  set onerror(f) {
+    this.impl_.onerror = f;
+  }
+
+  get readyState() {
+    return this.impl_.readyState;
+  }
+
+  /**
+  @param {String|ArrayBuffer|ArrayBufferView|Blob} msg
+  */
+  send(msg) {
+    this.impl_.send(msg);
+  };
+
+  /**
+  @param {number} [code]
+  @param {string} [reason]
+  */
+  close(code,reason) {
+    this.impl_.close(code, reason);
+  };
+}
 
 /**
 @static
