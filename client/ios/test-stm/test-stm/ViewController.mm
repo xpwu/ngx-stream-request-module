6d52a659d35fd983d432d2d1b4940783959c28da
xpwu <xpwu@xpwu.local>
2017-12-28 14:54:55 +0800

    双连接测试

--- client/ios/test-stm/test-stm/ViewController.mm
+++ client/ios/test-stm/test-stm/ViewController.mm
@@ -12,6 +12,7 @@
 
 @interface ViewController () {
   STMClient* client;
+  STMClient* client2;
 }
 
 @end
@@ -22,32 +23,54 @@
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
   client = [[STMClient alloc]init];
+  client2 = [[STMClient alloc]init];
+  
   [client setConnectHost:@"127.0.0.1" port:8000
                onSuccess:^{
-                 NSLog(@"connect successful");
+                 NSLog(@"client1 connect successful");
                }
                 onFailed:^(NSString * error) {
-                  NSLog(@"%@", error);
+                  NSLog(@"client1 %@", error);
+                }];
+  [client2 setConnectHost:@"127.0.0.1" port:8002
+               onSuccess:^{
+                 NSLog(@"client2 connect successful");
+               }
+                onFailed:^(NSString * error) {
+                  NSLog(@"client2 %@", error);
                 }];
   
   client.onpush = ^(NSData* data) {
-    NSLog(@"[push]---%@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+    NSLog(@"[client1 push]---%@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+  };
+  
+  client2.onpush = ^(NSData* data) {
+    NSLog(@"[client2 push]---%@"
+          , [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
   };
   
   [client addRequestBody:[@"flajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflaflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklf" dataUsingEncoding:NSUTF8StringEncoding] headers:@{@"api": @"/api/ServerInfo"} onSuccess:^(NSData * data) {
-    NSLog(@"success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+    NSLog(@"client1 success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
   } onFailed:^(NSString * str) {
-    NSLog(@"%@", str);
+    NSLog(@"client1 %@", str);
   } onComplete:^{
-    NSLog(@"complete");
+    NSLog(@"client1 complete");
   }];
   
   [client addRequestBody:[@"flajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflaflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklf" dataUsingEncoding:NSUTF8StringEncoding] headers:@{@"api": @"/api/php/info"} onSuccess:^(NSData * data) {
-    NSLog(@"success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+    NSLog(@"client1 success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
   } onFailed:^(NSString * str) {
-    NSLog(@"%@", str);
+    NSLog(@"client1 %@", str);
   } onComplete:^{
-    NSLog(@"complete");
+    NSLog(@"client1 complete");
+  }];
+  
+  [client2 addRequestBody:[@"client2" dataUsingEncoding:NSUTF8StringEncoding] headers:@{@"api": @"api/php/info"} onSuccess:^(NSData * data) {
+    NSLog(@"client2 success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+  } onFailed:^(NSString * str) {
+    NSLog(@"client2 %@", str);
+  } onComplete:^{
+    NSLog(@"client2 complete");
   }];
 }
 
@@ -58,12 +81,22 @@
 
 -(IBAction)selector:(id)sender {
   [client addRequestBody:[@"flajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflaflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklfjsdklflajsfjklsdfjklasdklf" dataUsingEncoding:NSUTF8StringEncoding] headers:@{@"api": @"/api/php/info"} onSuccess:^(NSData * data) {
-    NSLog(@"success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+    NSLog(@"client1 success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
   } onFailed:^(NSString * str) {
-    NSLog(@"%@", str);
+    NSLog(@"client1 %@", str);
   } onComplete:^{
-    NSLog(@"complete");
+    NSLog(@"client1 complete");
   }];
+  
+  
+  [client2 addRequestBody:[@"client2" dataUsingEncoding:NSUTF8StringEncoding] headers:@{@"api": @"api/php/info"} onSuccess:^(NSData * data) {
+    NSLog(@"client2 success, %@", [[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+  } onFailed:^(NSString * str) {
+    NSLog(@"client2 %@", str);
+  } onComplete:^{
+    NSLog(@"client2 complete");
+  }];
+  
 }
 
 @end
